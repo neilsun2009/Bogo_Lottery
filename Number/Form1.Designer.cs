6a8d84b7194c6a3a2e01d4aad2f014a5656478b6
neilsun2009 <neilsun2009@163.com>
2017-04-06 22:06:42 +0800

    program

---
+++ Number/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Number
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(12, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(724, 422);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "开始摇号";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("宋体", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label4.Location = new System.Drawing.Point(298, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 80);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "000";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "最小号码：";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(68, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 21);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(266, 12);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 21);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            38,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "最大号码：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(399, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "去除号码（使用半角逗号\',\'隔开）：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(596, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 21);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "7,23,30";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.numericUpDown1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.numericUpDown2);
+            this.panel2.Location = new System.Drawing.Point(9, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(731, 47);
+            this.panel2.TabIndex = 7;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(689, 480);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(47, 12);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "by Bogo";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(748, 501);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(764, 540);
+            this.MinimumSize = new System.Drawing.Size(764, 540);
+            this.Name = "Form1";
+            this.Text = "摇号 by Bogo";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Number/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Number
+{
+    public partial class Form1 : Form
+    {
+        // state: 0 - pending, 1 - rolling
+        private int state = 0;
+        private int rollingNumber = 0;
+        System.Threading.Timer timerThr;
+        private delegate void SetTBMethodInvoke(object state);
+
+        int min, max, exLength;
+        String[] ex;
+        int[] exInt;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timerThr = new System.Threading.Timer(new TimerCallback(SetTB), null, Timeout.Infinite, 500);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (state == 0)
+            {
+                min = (int)numericUpDown1.Value;
+                max = (int)numericUpDown2.Value;
+                if (min > max)
+                {
+                    MessageBox.Show("最小号码大于最大号吗！\n请重新填写！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                ex = textBox1.Text.Split(',');
+                exLength = ex.Length;
+                exInt = new int[exLength];
+                for (int i = 0; i < exLength; ++i)
+                {
+                    bool success = Int32.TryParse(ex[i], out exInt[i]);
+                    if (!success)
+                    {
+                        if (ex[i].Trim().Equals("")) {
+                            continue;
+                        }
+                        MessageBox.Show("去除号码出现非法字符“" + ex[i] + "”，请输入数字！\n本次摇号将忽略之！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        exInt[i] = -1;
+                    }
+                }
+                timerThr.Change(0, 10);
+                button1.Text = "摇号中";
+                state = 1;
+                panel2.Enabled = false;
+            }
+            else {
+                state = 0;
+                button1.Text = "开始摇号";
+                timerThr.Change(Timeout.Infinite, 10);
+                // Thread.Sleep(1000);
+                int x = getRandom();
+                Console.WriteLine("chose " + x);
+                label4.Text = getString(x);
+                panel2.Enabled = true;
+            }
+            
+            
+        }
+
+        private int getRealRandom(int min, int max) {
+            // Console.WriteLine(min + "-" + max);
+            return (int)Math.Floor(new Random().NextDouble() * (max - min + 1)) + min;
+        }
+
+        private int getRandom() {
+            int re;
+            while (true) {
+                re = getRealRandom(min, max);
+                bool cont = false;
+                for (int i = 0; i < exLength; ++i) {
+                    if (re == exInt[i])
+                    {
+                        cont = true;
+                        break;
+                    }
+                }
+                if (!cont) {
+                    break;
+                }
+            }
+            return re;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.bogobogo.cn");
+        }
+
+        private string getString(int x) {
+            string re = x.ToString();
+            if (x < 10)
+            {
+                re = "00" + re;
+            }
+            else if (x < 100) {
+                re = "0" + re;
+            }
+            return re;
+        }
+
+        public void SetTB(object value)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new SetTBMethodInvoke(SetTB), value);
+            }
+            else
+            {
+                rollingNumber = (int)Math.Floor(new Random().NextDouble() * 1000.0);
+                if (state == 1)
+                {
+                    label4.Text = getString(rollingNumber);
+                }
+            }
+        }
+    }
+}
